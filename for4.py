c1c0eca45032b0e9d426a207a74afcef77011b88
ism21775750 <ism21775750@a01.informatica.escoladeltreball.org>
2018-02-15 13:57:18 +0100

    first commit

--- for4.py
+++ for4.py
@@ -1,6 +1,15 @@
-#coding: utf8
-num=input("¿Cuantos valores va a introducir?")
 
+print "NUMEROS NEGATIVOS"
 
-while (num<0):
-	print("¡Imposible!")
+num=int(input("Cuantos valores va a introducir? "))
+aux=0
+
+while num<=0:
+	print "Imposible"
+	num=int(input("Intentalo de nuevo: "))
+	
+for i in range (1,num+1):
+	num2=int(input("Escriba el numero "+str(i)+": "))
+	if num2<0:
+		aux+=1
+print "Ha escrito "+str(aux)+" numeros negativos"
